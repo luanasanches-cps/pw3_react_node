6ddb513e865a4b7716d83a5052dc379eb135a85a
luanasanches-cps <luana.sanches01@etec.sp.gov.br>
2024-10-30 21:20:05 -0300

    Aula3010 - Hooks

--- minideiablog/src/hooks/useAuthentication.jsx
+++ minideiablog/src/hooks/useAuthentication.jsx
@@ -1,4 +1,4 @@
-import { db } from './../firebase/config'
+import { db } from '../firebase/config'
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -7,105 +7,104 @@ import {
     signOut
 } from 'firebase/auth'
 import { useState, useEffect } from 'react'
-
+ 
 export const useAuthentication = () => {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
     const [cancelled, setCancelled] = useState(false)
-
+ 
     const auth = getAuth()
-
-    function checkIsCancelled() {
+ 
+    function checkIfIsCancelled() {
         if (cancelled) {
             return
         }
     }
-
+ 
     async function createUser(data) {
-        checkIsCancelled()
-
+        checkIfIsCancelled()
+ 
         setLoading(true)
         setError(null)
-
+ 
         try {
             const { user } = await createUserWithEmailAndPassword(
                 auth,
                 data.email,
                 data.password
             )
-
             await updateProfile(user, {
                 displayName: data.displayName
             })
-
             setLoading(false)
+ 
             return user
-        } catch (error) {
-            console.error(error.message),
-                console.table(typeof error.message)
-
+        }catch(error){
+            console.error(error.message)
+            console.table(typeof error.message)
+ 
             let systemErrorMessage
-
-            if (error.message.includes('Password')) {
-                systemErrorMessage = 'A senha precisa conter ao menos 6 caracteres.'
-            } else if (error.message.includes('email-already')) {
-                systemErrorMessage = 'Este e-mail já existe em nossos sistemas'
-            } else {
-                systemErrorMessage = 'Tente mais tarde, nosso sistema está indisponível.'
+ 
+            if(error.message.include('Password')){
+                systemErrorMessage = "A senha precisa conter ao menos 6 caracteres."
+            }else if(error.message.include('email-already')){
+                systemErrorMessage = "E-mail já cadastrado em nosso sistema."
+            }else{
+                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde."
             }
-
+ 
             setLoading(false)
             setError(systemErrorMessage)
         }
     }
-
-    const login = async (data) => {
-        checkIsCancelled()
-
+ 
+    const login = async (data) =>{
+        checkIfIsCancelled()
+ 
         setLoading(true)
-        setError(false)
-
-        try {
+        setError(null)
+ 
+        try{
             await signInWithEmailAndPassword(
                 auth,
                 data.email,
                 data.password
             )
             setLoading(false)
-        } catch (error) {
+        }catch(error){
             console.error(error.message)
             console.table(typeof error.message)
-
+ 
             let systemErrorMessage
-
-            if (error.message.includes('invalid-login-credentials')) {
-                systemErrorMessage = 'Este usuário não existe em nosso sistemas.'
-            } else if (error.message.includes('wrong-password')) {
-                systemErrorMessage = 'Há algum erro com suas informações'
-            } else {
-                systemErrorMessage = 'Tente mais tarde, nosso sistema está indisponível.'
+ 
+            if(error.message.include('invalid-login-credentials')){
+                systemErrorMessage = "Este usuário não tem registro em nossos sistemas"
+            }else if(error.message.include('wrong-password')){
+                systemErrorMessage = "Existe algum erro em suas credenciais de login"
+            }else{
+                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde."
             }
-
+ 
             setLoading(false)
             setError(systemErrorMessage)
         }
     }
-
-    const logout = () =>{
-        checkIsCancelled()
+ 
+    const logout = ()=>{
+        checkIfIsCancelled()
         signOut(auth)
     }
-
+ 
     useEffect(() =>{
         return () => setCancelled(true)
     }, [])
-
-    return {
+ 
+    return{
         auth,
         createUser,
         error,
         loading,
-        login,
-        logout
+        logout,
+        login
     }
 }
