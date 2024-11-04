cb5d15acbd9370a21ec64ac1d6b437f7bbb6daed
luanasanches-cps <Luana.sanches01@etec.sp.gov.br>
2024-11-04 20:19:46 -0300

    Aula1104 - Deploy de CreatePost

--- minideiablog/src/pages/CreatePost/CreatePost.jsx
+++ minideiablog/src/pages/CreatePost/CreatePost.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
-import banner from "/banner_home.png"
 import style from './CreatePost.module.css'
+import { useState } from 'react';
+import { useInsertDocument } from './useInsertDocument'
+import { useNavigate } from 'react-router-dom'
+import { useAuthvalue } from './useAuthvalue'
 
 const CreatePost = () => {
   const [title, setTitle] = useState();
@@ -9,12 +11,110 @@ const CreatePost = () => {
   const [tags, setTags] = useState();
   const [formError, setFormError] = useState();
   
+const navigate = useNavigate()
+
+const { insertDocument, response } = useInsertDocument("posts")
+
+const handlerSubmit = (e) =>{
+  e.preventDefault();
+  setFormError("")
+
+  try{
+    new URL(image)
+  }catch(error){
+    setFormError("A imagem precisa ser uma URL.")
+  }
+
+  const tagArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+  if(!title || !image || !tags || !body){
+    setFormError("Por favor, preencha todos os campos para postagem!")
+  }
+
+  console.log(tagArray)
+
+  console.log(
+    {
+      title,
+      image,
+      body,
+      tags: tagArray,
+      uid: user.uid,
+      createBy: user.displayName
+    }
+  )
+
+  navigate("/")
+
+  if(formError) return
+
+  insertDocument (
+    {
+      title,
+      image,
+      body,
+      tags: tagArray,
+      uid: user.uid,
+      createBy: user.displayName
+    }
+  )
+}
   return (
     <>
-    <div>
-      <h1>Suas novas Ideias</h1>
-      <img src={banner} alt="Banner do Site" />
-    </div>
+<div className={styles.create_post}>
+        <h2>Nova Postagem</h2>
+        <p>Compartilhe sua experiência no mundo desenvolvedor</p>
+        <form onSubmit={handlerSubmit}>
+          <label>
+            <span>Título:</span>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Título da postagem"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required />
+          </label>
+          <label>
+            <span>URL da imagem:</span>
+            <input
+              type="text"
+              name="image"
+              id="image"
+              placeholder="Endereço da imagem da postagem"
+              onChange={(e) => setImage(e.target.value)}
+              value={image}
+              required />
+          </label>
+          <label>
+            <span>Conteúdo da Postagem:</span>
+            <textarea
+              name="body"
+              id="body"
+              placeholder="Insira o conteúdo de sua postagem aqui"
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              required>
+            </textarea>
+          </label>
+          <label>
+            <span>Tags:</span>
+            <input
+              type="text"
+              name="tags"
+              id="tags"
+              placeholder="Insira suas tags separadas por virgulas"
+              onChange={(e) => setTags(e.target.value)}
+              value={tags}
+              required />
+          </label>
+            {!response.loading && <button className="btn">Criar Postagem</button>}
+            {response.loading && <button className="btn" disabled>Postando...</button>}
+            {(response.error || formError) && <p className='error'>{error || formError}</p>}
+        </form>
+      </div>
+
     </>
   )
 }
